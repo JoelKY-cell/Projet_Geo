e6df575864255476972ff01a29a646f2153b6320
JoelKY-cell <joelkyras3@gmail.com>
2026-02-06 09:32:55 +0000

    Mise à jour des fichiers urls

--- backend/plugins/tracking/urls.py
+++ backend/plugins/tracking/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import PositionViewSet, TripViewSet, StopPointViewSet
 
 router = DefaultRouter()
+router.include_format_suffixes = False
 router.register(r'positions', PositionViewSet)
 router.register(r'trips', TripViewSet)
 router.register(r'stops', StopPointViewSet)
--- backend/plugins/vehicles/urls.py
+++ backend/plugins/vehicles/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import VehicleViewSet
 
 router = DefaultRouter()
+router.include_format_suffixes = False
 router.register(r'', VehicleViewSet)
 
 urlpatterns = [
